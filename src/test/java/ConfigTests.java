78a77a9a77dcf5ccc0b688477438da83419907a3
Vova Buligini <buligini2vova@gmail.com>
2022-12-20 19:48:25 +0400

    configTest update

--- src/test/java/ConfigTests.java
+++ src/test/java/ConfigTests.java
@@ -2,10 +2,13 @@ import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import static com.codeborne.selenide.Configuration.holdBrowserOpen;
 import static com.codeborne.selenide.Configuration.timeout;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class ConfigTests {
     @BeforeSuite
@@ -16,5 +19,9 @@ public class ConfigTests {
         WebDriverRunner.setWebDriver(driver);
         holdBrowserOpen = false;
         timeout = 5000;
+    }
+    @AfterMethod
+    public void Close (){
+        closeWebDriver();
     }
 }
